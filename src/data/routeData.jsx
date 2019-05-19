515877922b08a9d5ba87bd4ba6bc4806c2565d53
maniekpl00 <maniek11pl@gmail.com>
2019-05-19 19:31:28 +0200

    Finish exercise3

--- src/data/routeData.jsx
+++ src/data/routeData.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import Exercise1 from '../exercises/Exercise1/Exercise1';
 import Exercise2 from '../exercises/Exercise2/Exercise2';
+import Exercise3 from '../exercises/Exercise3/Exercise3';
 
-const REPOSITORY_URL = 'https://github.com/maniekpl00/react-learning/src/exercises';
+const REPOSITORY_URL = 'https://github.com/maniekpl00/react-learning/tree/master/src/exercises';
 
 const buildRoute = (name, path, component, repositoryURL, description) => {
   return {
@@ -25,5 +26,22 @@ export default [
       <em>React Hooks, React.Memo, useState, useMemo, PropTypes</em>.
     </>
   ),
-  buildRoute('Exercise 2', '/exercise2', Exercise2, `${REPOSITORY_URL}/Exercise2`, 'Opis zad2')
+  buildRoute(
+    'Web (Http / Ajax) ',
+    '/exercise2',
+    Exercise2,
+    `${REPOSITORY_URL}/Exercise2`,
+    <>
+      Using <em>Http, Ajax, Axios Interceptors (request, response),</em>
+    </>
+  ),
+  buildRoute(
+    'Animate members',
+    '/exercise3',
+    Exercise3,
+    `${REPOSITORY_URL}/Exercise3`,
+    <>
+      Using animation: scale, translate, opacity
+    </>
+  )
 ];
